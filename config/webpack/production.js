eda0722de6ca2f793851e04c10e2374f8778f272
Michael Deitcher <michaeldeitcher@gmail.com>
2021-11-09 10:15:09 -0700

    setting api root

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env['API_ROOT'] = 'https://nameless-eyrie-18132.herokuapp.com/'
 
 const environment = require('./environment')
 
