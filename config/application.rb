4701cd1091a0985823568bf64a0e6a704b913ba4
Michael Deitcher <michaeldeitcher@gmail.com>
2021-11-09 09:51:42 -0700

    Only use dotenv for development

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Railtie.load if Rails.env.development?
 
 module SpotAmusic
   class Application < Rails::Application
--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,1 +1,3 @@
-Dotenv.require_keys("SPOTIFY_CLIENT_ID", "SPOTIFY_REDIRECT_URI", "SPOTIFY_CLIENT_SECRET")
+if Rails.env.development?
+  Dotenv.require_keys("SPOTIFY_CLIENT_ID", "SPOTIFY_REDIRECT_URI", "SPOTIFY_CLIENT_SECRET")
+end
